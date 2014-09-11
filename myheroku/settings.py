07f4edda7fbaf29f478615b7e971dc8601e28cb4
Dennis Zellich <dennis.zellich@gmail.com>
2014-09-11 21:55:09 +0200

    Second commit database added

--- myheroku/settings.py
+++ myheroku/settings.py
@@ -2,7 +2,7 @@
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -13,13 +13,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        
     }
 }
 
@@ -157,6 +152,11 @@ LOGGING = {
     }
 }
 
-ALLOWED_HOSTS = ['myheroku2.herokuapp.com']
+ALLOWED_HOSTS = ['herokumarina.herokuapp.com']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+try:
+  from local_settings import *
+except Exception as e:
+  pass
