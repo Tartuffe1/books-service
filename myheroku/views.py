508814db8a73370ec61576d18459e54fd41d7432
Dennis Zellich <dennis.zellich@gmail.com>
2014-11-02 08:25:05 +0100

    Bootstrap added

--- myheroku/views.py
+++ myheroku/views.py
@@ -2,4 +2,4 @@
 from django.shortcuts import render_to_response
 from django.shortcuts import RequestContext
 def home(request):
-    return render_to_response("base.html", RequestContext(request, {}))
+    return render_to_response("home.html", RequestContext(request, {}))
