76258da34c53d9a7c81dc1e10fd0d655b6aa90ca
Dennis Zellich <dennis.zellich@gmail.com>
2014-11-10 09:16:56 +0100

    test.py extended

--- accounts/tests.py
+++ accounts/tests.py
@@ -14,3 +14,12 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+from userena.models import UserenaSignup
+
+class MyViewTestCase(TestCase):
+    def test_login(self):
+        my_user = UserenaSignup.objects.create_user(username="my_name", email="my_name@gmail.com", password="my_password")
+        active_my_user = UserenaSignup.objects.activate_user(my_user.userena_signup.activation_key)
+        log_in = self.client.login(username=active_my_user.username, password="my_password", email= active_my_user.email)
+        self.assertEqual(log_in, True)      
