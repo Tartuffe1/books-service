6acce20cfa754dd0975379fcf671a8de48acca20
Dennis Zellich <dennis.zellich@gmail.com>
2014-11-24 08:53:08 +0100

    Dotjerivanje izgleda poetne stranice, spajanje na bazu podataka i prikaz knjiga iz nje(views.py promjena)

--- myheroku/views.py
+++ myheroku/views.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
+from books.models import Book
+
 def home(request):
     args={}
+    latest_book_list = Book.objects.all()
+    args['latest_book_list']=latest_book_list
     args['context_instance']=RequestContext(request)
     return render(request, 'home.html', args)
+    
