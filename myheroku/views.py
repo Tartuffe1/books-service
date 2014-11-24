5cd0fd083f0d77d6c5aaa0792a4eb8b8fcc8daa2
Dennis Zellich <dennis.zellich@gmail.com>
2014-11-24 09:31:44 +0100

    Pagination added to home.html

--- myheroku/views.py
+++ myheroku/views.py
@@ -3,10 +3,26 @@ from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
 from books.models import Book
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 def home(request):
     args={}
-    latest_book_list = Book.objects.all()
-    args['latest_book_list']=latest_book_list
     args['context_instance']=RequestContext(request)
+    
+    latest_book_list = Book.objects.all()
+    paginator = Paginator(latest_book_list, 4) # Show 4 contacts per page
+
+    # this value is delivered by a href='?page=..., obviously it is GET method   
+    page = request.GET.get('page')
+    try:
+       books = paginator.page(page)
+    except PageNotAnInteger:
+       # If page is not an integer, deliver first page.
+       books = paginator.page(1)
+    except EmptyPage:
+       # If page is out of range (e.g. 9999), deliver last page of results.
+       books = paginator.page(paginator.num_pages)
+        
+    args['book_list']=books
     return render(request, 'home.html', args)
     
